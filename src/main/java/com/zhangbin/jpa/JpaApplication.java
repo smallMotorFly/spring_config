1672ed0ea5fc2f704c5d7891fbbbe1431a12215e
zhangbin <bin.zhang@juneyaokc.com>
2019-01-23 22:36:45 +0800

    经过之前一个牛逼的同事查找，终于找到了问题，server=8080没有写。大写的尴尬，还有application需要加载页面指定，yml中的配置不起作用。待查找问题

--- src/main/java/com/zhangbin/jpa/JpaApplication.java
+++ src/main/java/com/zhangbin/jpa/JpaApplication.java
@@ -2,12 +2,21 @@ package com.zhangbin.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class JpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
+    }
+    @Bean
+    public InternalResourceViewResolver setupViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
     }
 
 }
